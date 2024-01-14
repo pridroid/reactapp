77d220e1afdf07d4251361dc7b54003406e30d12
pridroid <priyalkalal2693@gmail.com>
2024-01-14 23:44:36 +0530

    nested browser router inside AuthOProvider -  main.jsx

--- clone/src/main.jsx
+++ clone/src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Auth0Provider
-      domain="dev-5vsv8xdmudbqc4wa.us.auth0.com"
-      clientId="yFIrf644Mrc4naexSK36Iicz1ojTTmya"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-5vsv8xdmudbqc4wa.us.auth0.com"
+    clientId="yFIrf644Mrc4naexSK36Iicz1ojTTmya"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <BrowserRouter>
       <App />
-    </Auth0Provider>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </Auth0Provider>
 );
